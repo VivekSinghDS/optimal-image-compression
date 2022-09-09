e7af38f36ac328d2d5e1ef13db838412e45c590a
Vivek Singh <92970332+VivekSinghDS@users.noreply.github.com>
2022-09-09 16:30:43 +0530

    Update host_check.py

--- host_check.py
+++ host_check.py
@@ -1,8 +1,8 @@
 import requests ## to use the POST method we use a library named requests
-# url = 'http://0.0.0.0:5000/predict'
-url= "http://ae0b3a91bc8b543558bbddc1e7947d31-594062275.us-east-1.elb.amazonaws.com:5000/predict" # eks deployment
+# url = 'http://0.0.0.0:5000/predict' # uncomment if you want to test it locally 
+url= "" # eks deployment url here
 # url = "http://optimalimage8-env.eba-umrpekgu.us-east-2.elasticbeanstalk.com/predict" # aws beanstalk deployment
-url = "http://ec2-44-204-227-255.compute-1.amazonaws.com:80/predict"
+# url = "http://ec2-44-204-227-255.compute-1.amazonaws.com:80/predict"
 # url = "http://127.0.0.1:8080/predict"
 url_r = {
     "image_path":"https://images.contentstack.io/v3/assets/blt5ac836f818fcdeb2/blt588212a6b0619c3d/62d4ed04a80c6d36eaa140a7/sample20.jpg"
